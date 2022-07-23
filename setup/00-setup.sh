389e16dd961e3dafbc1832f5b78cf92268b8cf63
David Coulon <davcln@gmail.com>
2022-07-24 00:43:31 +0200

    reorg

---
+++ setup/00-setup.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+sudo su - postgres
+createuser farmer
+createdb  farming
---
+++ setup/01-database.sql
@@ -0,0 +1,2 @@
+alter user farmer with encrypted password 'farmer';
+grant all privileges on database farming to farmer;
